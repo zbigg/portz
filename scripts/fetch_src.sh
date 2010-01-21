5ecfb5c49646b5efc61b744aa560d421bc85fe88
zbigg <zbigg@zbigg.org>
2010-01-21 23:47:59 +0000

    portz_invoke: added 'fetch_src' target

---
+++ scripts/fetch_src.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# fetch and unarchive
+. ${portz_root}/scripts/defs.sh
+
+archive_file=$(portz_step $(pwd) fetch ${baseurl})
+inform archive_file="$archive_file"
+rm -rf ${TMP}/portz/${package}/src
+portz_step ${TMP}/portz/${package}/src unarchive ${archive_file}
--- scripts/install_staging.sh
+++ scripts/install_staging.sh
@@ -4,10 +4,7 @@
 
 # fetch and unarchive
 
-archive_file=$(portz_step $(pwd) fetch ${baseurl})
-inform archive_file="$archive_file"
-rm -rf ${TMP}/portz/${package}/src
-portz_step ${TMP}/portz/${package}/src unarchive ${archive_file}
+${portz_root}/scripts/fetch_src.sh
 
 # find dirs
 

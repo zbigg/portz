ee5895dd935d84c099351f33605d1cab53223834
z.zagorski <z.zagorski@gmail.com>
2012-03-02 21:35:19 +0000

    simple support for svn

--- scripts/fetch_src.sh
+++ scripts/fetch_src.sh
@@ -3,8 +3,23 @@
 # fetch and unarchive
 . ${portz_root}/scripts/defs.sh
 
-archive_file=$(portz_step $(pwd) fetch ${baseurl})
+src_dir="${TMP}/portz/${package}/src"
+rm -rf ${src_dir}
+mkdir -p ${src_dir}
 
-inform archive_file="$archive_file"
-rm -rf ${TMP}/portz/${package}/src
-portz_step ${TMP}/portz/${package}/src unarchive ${archive_file}
+if [ -n "${svn_path}" ] ; then
+	dir="$package-trunk"
+	if [ -n "${revision}" ] ; then
+		svn_options="-r ${revision}"
+		dir="${package}-r${revision}"
+	fi
+	(
+		cd ${src_dir}
+		portz_invoke svn co ${svn_options} ${svn_path} ${dir}
+	)
+else
+	archive_file=$(portz_step $(pwd) fetch ${baseurl})
+
+	inform archive_file="$archive_file"
+	portz_step ${TMP}/portz/${package}/src unarchive ${archive_file}
+fi
